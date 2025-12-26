0283405d9dfeeb3b656c6a9c569f51333b90f781
Novikov <ruslan.n@hypecharge.io>
2025-12-26 11:10:18 +0200

    Separate HOMEPAGE template

---
+++ themes/couplescompatibility/template-homepage.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template Name: Homepage
+ * Description: Landing page template with ACF fields for homepage sections
+ */
+
+$context = Timber\Timber::context();
+$context['post'] = Timber\Timber::get_post();
+
+// ACF Integration - add fields to context if ACF is active
+// Polylang automatically handles field translations
+if (function_exists('get_fields')) {
+	$context['fields'] = get_fields();
+}
+
+// Get 4 random posts for blog preview (Polylang filters by current language)
+$posts = Timber\Timber::get_posts([
+	'post_type' => 'post',
+	'posts_per_page' => 4,
+	'orderby' => 'rand',
+]);
+$context['blog_posts'] = [];
+foreach ($posts as $post) {
+	$context['blog_posts'][] = [
+		'title' => $post->title(),
+		'link' => $post->link(),
+		'preview' => wp_trim_words(wp_strip_all_tags($post->content()), 25, '...'),
+	];
+}
+
+// Render landing page template
+Timber\Timber::render('pages/landing.twig', $context);
